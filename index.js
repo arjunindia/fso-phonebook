0a6e028c138553798735658af2578d6a3aad27e7
Arjun <arjun8107@gmail.com>
2024-09-12 00:05:12 +0530

    added logginh

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import express from "express";
+import morgan from "morgan";
 const app = express();
 app.use(express.json());
+
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+);
 
 let persons = [
   {
